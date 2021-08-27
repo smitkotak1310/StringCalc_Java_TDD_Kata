b61ae412152fa9721df91a4aacba4a33f5881d30
smitkotak1310 <43035695+smitkotak1310@users.noreply.github.com>
2021-08-27 22:41:14 +0530

    Created Add Method and 2 Test Cases for TestingStringCalculator

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,4 +1,38 @@
+import java.util.Vector;
 
 public class StringCalculator {
+
+	public int Add(String numbers)
+	{
+		String[] str = numbers.split(",|\n");  
+		if(numbers.isEmpty())
+		{
+			return 0;
+		}
+		if(numbers.length()==1)
+		{
+
+			return StringtoInt(numbers);
+		}
+		else
+		{
+			return getSum(str[0],str[1]);
+		}
+	}
+	
+	private int getSum(String num1, String num2)
+	{
+		return Integer.parseInt(num1)+Integer.parseInt(num2);
+	}
+	
+	private boolean isEmpty(String numbers)
+	{
+		return numbers.isEmpty();
+	}
+	
+	private int StringtoInt(String numbers)
+	{
+		return Integer.parseInt(numbers);
+	}
 
 }
--- src/TestingStingCalculator.java
+++ src/TestingStingCalculator.java
@@ -1,8 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class TestingStingCalculator {
 	
 	StringCalculator strcalc = new StringCalculator();
 	
+	@Test
+	public void zeroOnEmpty()
+	{
+		assertEquals(0,strcalc.Add(""));
+	}
+	
+	@Test
+	public void returnSumOnTwoArgsDelimitedByComma()
+	{
+		assertEquals(3, strcalc.Add("1,2"));
+	}
 	
 	
 }
